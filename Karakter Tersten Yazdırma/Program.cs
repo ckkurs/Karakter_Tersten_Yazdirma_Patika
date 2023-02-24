ad0ddc1f008fe96d08072e93b96c6b3bb147410b
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-24 20:41:24 +0300

    Karakter_Tersten_Yazdirma_Patika

---
+++ Karakter Tersten Yazdırma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Karakter_Tersten_Yazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir metin girin: ");
+            string metin = Console.ReadLine();
+            string output = "";
+
+            for (int i = metin.Length - 1; i >= 0; i--)
+            {
+                output = output + metin[i];
+            }
+
+            Console.WriteLine("Ters çevrilmiş metin: " + output);
+            Console.ReadLine();
+        }
+    }
+}
